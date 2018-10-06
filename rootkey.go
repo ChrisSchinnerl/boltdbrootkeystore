77b593c4a3e7d0b018133570673fa2cbad37c5cb
Chris <ch.schinnerl@gmail.com>
2018-10-05 22:06:31 -0400

    initial commit

---
+++ rootkey.go
@@ -0,0 +1,66 @@
+package boltdbrootkeystore
+
+import (
+	"sync"
+	"time"
+
+	bolt "github.com/coreos/bbolt"
+	"gopkg.in/macaroon-bakery.v2/bakery"
+	"gopkg.in/macaroon-bakery.v2/bakery/dbrootkeystore"
+)
+
+var (
+	clock      dbrootkeystore.Clock
+	newBacking = func(s *RootKeys) dbrootkeystore.Backing {
+		return backing{s}
+	}
+)
+
+// Policy holds a store policy for root keys.
+type Policy dbrootkeystore.Policy
+
+// RootKeys represents a cache of macaroon root keys.
+type RootKeys struct {
+	keys *dbrootkeystore.RootKeys
+
+	db     *bolt.DB
+	bucket []byte
+
+	// initDBOnce guards initDBErr.
+	initDBOnce sync.Once
+	initDBErr  error
+}
+
+func NewRootKeys(db *bolt.DB, bucket []byte, maxCacheSize int) *RootKeys {
+	return &RootKeys{
+		keys:   dbrootkeystore.NewRootKeys(maxCacheSize, clock),
+		db:     db,
+		bucket: bucket,
+	}
+}
+
+func (s *RootKeys) NewStore(policy Policy) bakery.RootKeyStore {
+	b := newBacking(s)
+	return s.keys.NewStore(b, dbrootkeystore.Policy(policy))
+}
+
+// backing implements dbrootkeystore.Backing by using Postgres as
+// a backing store.
+type backing struct {
+	keys *RootKeys
+}
+
+// GetKey implements dbrootkeystore.Backing.GetKey.
+func (b backing) GetKey(id []byte) (dbrootkeystore.RootKey, error) {
+	return b.keys.getKey(id)
+}
+
+// InsertKey implements dbrootkeystore.Backing.InsertKey.
+func (b backing) InsertKey(key dbrootkeystore.RootKey) error {
+	return b.keys.insertKey(key)
+}
+
+// FindLatestKey implements dbrootkeystore.Backing.FindLatestKey.
+func (b backing) FindLatestKey(createdAfter, expiresAfter, expiresBefore time.Time) (dbrootkeystore.RootKey, error) {
+	return b.keys.findLatestKey(createdAfter, expiresAfter, expiresBefore)
+}
